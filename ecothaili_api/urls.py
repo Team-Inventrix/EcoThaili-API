d273046c23f89f189bc7e94c3aaa31c9e2523e01
destny69 <mzldestiny69@gmail.com>
2024-02-15 00:20:08 +0545

    fixes: fix the error in settings.py file

--- ecothaili_api/urls.py
+++ ecothaili_api/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-     path('cms/', include("cms.urls")),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
