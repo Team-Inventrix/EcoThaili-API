327774151e1220feff03610a04fdf588451bc011
destny69 <mzldestiny69@gmail.com>
2024-02-14 23:49:04 +0545

    feature: Addition of Product model in CMS

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+
+
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product_name = models.CharField(max_length=20, null=False, blank = False)
+    Description = models.TextField(blank = False, null = False)
+    price = models.PositiveSmallIntegerField(blank = False, null = False)
+    image = models.ImageField(upload_to='product_images/')
+
+
+    def __str__(self):
+        return f'{self.product_name}'   
--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
+
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Product
+from .serializers import ProductSerializer
 
-from django.http import HttpResponse
-
-
-def eco_api(request):
-    return HttpResponse("Hello, EcoApi!!")
+class ProductViewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
