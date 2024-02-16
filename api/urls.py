894b3990433f33e54c8bf33074fa6d302d7cc992
Kshitiz Mhto <kshitizmhto101@gmail.com>
2024-02-16 20:21:04 +0545

    feat: added necessary url path

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from .views import ProductViewset
 from rest_framework import  routers
-
+from django.apps import apps 
 
 
 router = routers.DefaultRouter()
@@ -10,4 +10,6 @@ router.register(r'products',ProductViewset,basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('marketplace/', include(apps.get_app_config('oscar').urls[0])),
 ]
