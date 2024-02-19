329be37bbef796df0a3026c4d4cb80f71dc72d43
destny69 <mzldestiny69@gmail.com>
2024-02-19 19:08:21 +0545

    docs: added documentation

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ from api.models import User, Product, Subscriber, Careers
 from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from api.utils import Util
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
   # We are writing this becoz we need confirm password field in our Registratin Request
